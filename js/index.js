36df161e201cc594a8d4d7fe851c7608ff328b65
Stephanie Hill <stephni.hill@gmail.com>
2023-01-02 18:02:49 -0800

    score and food

--- js/index.js
+++ js/index.js
@@ -14,13 +14,23 @@ let snake = [
   { x: 160, y: 200 },
 ];
 
+let score = 0;
+
+// will be true if changing direction
+let changingDirection = false;
+
+// initialize variables for food to be generated
+let foodX;
+let foodY;
+
 // dx is the x-axis coordinate in the destination canvas at which to place the top-left corner of the source image
 // dy is the y-axis coordinate in the destination canvas
 let dx = 10;
 let dy = 0;
 
-//start game by hoisting main function
+//start game by hoisting main function and generating food
 main();
+generateFood();
 
 // we need an event listener to listen to when the key is pressed
 document.addEventListener('keydown', changeDirection);
@@ -30,48 +40,89 @@ function main() {
 
   // hoist endGame function to stop play if snake hits sides of board
   if (endGame()) return;
-  changeDirection = false;
+  changingDirection = false;
 
   // utilizing setTimeout to auromatically move the snake, but with a slight delay so that the snake moves more fluidly
   // we also recursively call our main function so that the movement will continue
   setTimeout(function onTick() {
     reset();
+    drawFood();
     moveSnake();
     drawSnake();
     main();
   }, 100);
-}
+};
 
-// draws the canvas
+// draws the game board canvas
 function reset() {
-
   gameBoard_ctx.fillStyle = 'rgb(237, 237, 240)';
   gameBoard_ctx.strokeStyle = 'rgb(0, 0, 3)';
   gameBoard_ctx.fillRect(0, 0, gameBoard.width, gameBoard.height);
   gameBoard_ctx.strokeRect(0, 0, gameBoard.width, gameBoard.height);
-
 };
 
-// function to display on the canvas, drawing a rectangle for each set of coordinates
-function createSnake(snakePart) {
-
-  gameBoard_ctx.fillStyle = 'rgb(110, 240, 128)';
-  gameBoard_ctx.strokeStyle = 'rgb(2, 38, 18)';
-  gameBoard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-  gameBoard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-
-};
-
-// function to print the parts
+// function to display the snake on the canvas
 function drawSnake() {
   snake.forEach(createSnake);
 };
 
+// function to display on the canvas, drawing a rectangle for each set of coordinates
+function createSnake(snakePart) {
+  gameBoard_ctx.fillStyle = 'rgb(110, 240, 128)';
+  gameBoard_ctx.strokeStyle = 'rgb(2, 38, 18)';
+  gameBoard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+  gameBoard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+};
+
+// function to display food on canvas 
+function drawFood() {
+  gameBoard_ctx.fillStyle = 'rgb(230, 87, 139)';
+  gameBoard_ctx.strokeStyle = 'rgb(122, 5, 32)';
+  gameBoard_ctx.fillRect(foodX, foodY, 10, 10);
+  gameBoard_ctx.strokeRect(foodX, foodY, 10, 10);
+}
+
+// function to generate random coordinates for the position of food for our snake to eat
+function randomFood(min, max) {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+}
+
+// generates food using the random coordinates
+// ensures that the food is not being generated at the same position as the snake
+// determines if the snake has eaten the food based on coordinate overlap
+function generateFood() {
+  foodX = randomFood(1, gameBoard.width - 10);
+  foodY = randomFood(1, gameBoard.height - 10);
+
+  snake.forEach(function isFoodEaten(part) {
+    const wasEaten = part.x == foodX && part.y == foodY;
+    if (wasEaten) {
+      generateFood();
+    }
+  });
+};
+
 // here we create a new head and tehn  add the new head to the beginning, thus moving the snake: this is our horizontal movement
 function moveSnake() {
+
   const head = { x: snake[0].x + dx, y: snake[0].y + dy };
+
   snake.unshift(head);
-  snake.pop();
+
+  // if snake has eaten, we add a new "head" growing the snake
+  // otherwise we remove "head" reducing size of snake
+  const snakeHasEaten = snake[0].x === foodX && snake[0].y === foodY;
+  if (snakeHasEaten) {
+    //increase score
+    score += 10;
+    //display score
+    document.getElementById('score').innerHTML = score;
+    // generate more food
+    generateFood();
+  } else {
+    snake.pop();
+  }
+
 };
 
 // function allows us to move snake via keyboard
@@ -82,6 +133,9 @@ function changeDirection(event) {
   const RIGHT_KEY = 39;
   const UP_KEY = 38;
   const DOWN_KEY = 40;
+
+  if (changingDirection) return;
+  changingDirection = true;
 
   const keyPressed = event.keyCode;
   const goingUp = dy === -10;
